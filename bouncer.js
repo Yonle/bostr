1206f592f52d1c9ab36d5654ef11befc5ee91f6c
Yonle <yonle@lecturify.net>
2024-02-03 15:59:58 +0700

    fix commit 568ea5b

--- bouncer.js
+++ bouncer.js
@@ -361,7 +361,7 @@ function newConn(addr, id, reconn_t = 0) {
         // If it's at the limit, Send EOSE to client and delete pendingEOSE of subID
 
         // Skip if EOSE has been omitted
-        if (!client.pendingEOSE.has(data[1]) || !cFilter?.limit || !cFilter?.ids?.length || client.pause_subs.has(data[1])) return;
+        if (!client.pendingEOSE.has(data[1]) || !(cFilter?.limit || cFilter?.ids?.length) || client.pause_subs.has(data[1])) return;
         if (client.events.get(data[1]).size >= (cFilter?.ids?.length || cFilter?.limit)) {
           // Once reached to <filter.limit>, send EOSE to client.
           client.send(JSON.stringify(["EOSE", data[1]]));
